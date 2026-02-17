02502dc2755c55a06aa8548655770677f4a5470f
Enid <edotadot2@gmail.com>
2026-02-16 17:58:57 -0800

    add: intro component

--- min-portfolio/src/components/Intro.jsx
+++ min-portfolio/src/components/Intro.jsx
@@ -5,8 +5,8 @@ function Intro() {
         <div className="flex items-center 
         justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Enid</h1>
-            <p>Software Engineer</p>
-            <p>This is my bio</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">This is my bio</p>
         </div>
     )
 
