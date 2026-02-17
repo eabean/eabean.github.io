1266af2172d8c9cd06da27236aa857461ed09289
Enid <edotadot2@gmail.com>
2026-02-16 17:58:43 -0800

    add: portfolio component

---
+++ min-portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "RunBuddies",
+    imgUrl: "",
+    stack: ["C#", "PostgreSQL"],
+    link: "",
+  },
+  {
+    title: "My Portfolio",
+    imgUrl: "",
+    stack: ["React", "Tailwind", "Vite"],
+    link: "",
+  },
+  {
+    title: "Support Ticket Tagger",
+    imgUrl: "",
+    stack: ["C#", "Python"],
+    link: "",
+  },
+]
