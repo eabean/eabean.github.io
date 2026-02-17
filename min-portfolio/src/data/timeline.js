8d0976b331e084118f3a9b607b8663ef56b3591a
Enid <edotadot2@gmail.com>
2026-02-16 17:59:09 -0800

    add timeline component

---
+++ min-portfolio/src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    year: "Today",
+    title: "Systems Developer",
+    duration: "4 years - Now",
+    details: "Working full time.",
+  },
+  {
+    year: "2021",
+    title: "Software Developer",
+    duration: "5 months",
+    details: "Working full time",
+  },
+  {
+    year: "2021",
+    title: "Graduated",
+    duration: "6 years",
+    details: "Working full time",
+  },
+]
